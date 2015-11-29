dc349300daf00d136f893cad912a0fcd08e17fed
mrichins <richins.mason.a@gmail.com>
2015-11-28 19:18:12 -0500

    angular ng-repeat functional

--- public/app.js
+++ public/app.js
@@ -1,1 +1,5 @@
-var whatever = angular.module('whatever', []);
+var masonTest = angular.module('masonTest', []);
+
+masonTest.controller('testController', function(){
+	this.cars = [{'name':"car1"},{'name':"car2"},{'name':"car3"}];
+});
